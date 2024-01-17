0d728833e0b548942be4ea922face90daa4971a4
IsharaDayasena <141900612+IsharaDayasena@users.noreply.github.com>
2024-01-17 11:29:03 +0530

    Task_Management

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tasks,setTasks] = useState({
+    title: "",
+    date: "",
+    status: "",
+    priority: "",
+  });
+
+  console.log(tasks);
+  const handleOnChange = (e) =>{
+    setTasks({...tasks, [e.trget.name]: e.target.value});
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='h-screen w-auto bg-red-300'>
+        <div className='flex items-center justify-between'>
+          <div className='flex flex-col'>
+            <label>Task</label>
+          <input name="title"
+           onChange={(e)=> handleOnChange(e)} //onBlur use klama type krna eka console eke ewelem wdinne na eken ayn unama wdinw 
+           value={tasks.title}
+           placeholder='task'/>
+
+          </div>
+        
+
+        </div>
+
+      </div>
+    
+
     </div>
   );
 }
